a51b345966f97e6dd05090e7f13b566251a8e5da
VictorSzk <victor2308szk@gmail.com>
2025-02-17 16:00:01 -0300

    creating dist folder

---
+++ dist/_next/static/chunks/app/about/page-f0faa560d46e24a4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{7309:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,130,23))},130:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[171,441,517,358],()=>s(7309)),_N_E=e.O()}]);
